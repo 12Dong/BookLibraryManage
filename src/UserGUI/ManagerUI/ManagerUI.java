8aee9a02388c052f201ec401fab8902eae3f15c8
ThereWillBeOneDaypyf <527908203@qq.com>
2018-03-03 13:31:56 +0800

    update UI

--- src/UserGUI/ManagerUI/ManagerUI.java
+++ src/UserGUI/ManagerUI/ManagerUI.java
@@ -1,8 +1,13 @@
 package UserGUI.ManagerUI;
 
 
+import SQLQuery.Base.SQLBase;
+import UserRelated.user;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class ManagerUI {
     public ManagerUI(boolean is_supper_manager){
@@ -315,5 +320,39 @@ class ManagerFrame extends JFrame {
 //        layout.setConstraints(box2,s);
     }
 }
+
+class QueryListener implements MouseListener{
+    ManagerFrame workArea;
+    user 
+    public QueryListener(ManagerFrame myFrame){
+        this.workArea= myFrame;
+    }
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+
+    }
+}
+
+
 
 
