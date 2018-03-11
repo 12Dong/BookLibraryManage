3d120d78e918a3f65ebd3c1fd3e00af8673bed21
ThereWillBeOneDaypyf <527908203@qq.com>
2018-03-11 19:49:57 +0800

    2018年03月11日 19:49:56

--- src/GUI/login_GUI/Register_GUI/main.java
+++
@@ -1,7 +0,0 @@
-package GUI.login_GUI.Register_GUI;
-
-public class main {
-    public static void main(String argv[]){
-        new UserRegister();
-    }
-}
--- src/GUI/login_GUI/main.java
+++
@@ -1,7 +0,0 @@
-package GUI.login_GUI;
-
-public class main {
-    public static void main(String[] argv){
-        new login();
-    }
-}
